b8bb05994387dbfbd6872bde5cd94cb2d9580471
Ivan Zviagin <ivan@ivan.pw>
2020-08-15 15:42:35 +0300

    lesson 78

---
+++ lesson78 - add jquery/src/js/main.js
@@ -0,0 +1,11 @@
+export const one = 1;
+
+const two = 2;
+
+export {two};
+
+// export function sayHi() {
+export default function sayHi() {
+  console.log('Hi');
+}
+
---
+++ lesson78 - add jquery/src/js/script.js
@@ -0,0 +1,13 @@
+// import {one as first, two} from './main';
+// import * as data from './main';
+// import sayHi from './main';
+import $ from 'jquery';
+import * as data from './main.js';
+import sayHi from './main.js';
+
+// console.log(first + ' and ' + two);
+console.log(data.one + ' and ' + data.two);
+
+// data.sayHi();
+sayHi();
+
