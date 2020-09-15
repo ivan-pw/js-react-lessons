f5b59b5b8ddcf026efc019786cb6c94858c6215b
Ivan Zviagin <ivan@ivan.pw>
2020-09-15 16:14:34 +0300

    lesson 87

---
+++ lesson87 - classes/src/button.js
@@ -0,0 +1,40 @@
+function button() {
+    return 'button';
+  }
+  
+  class Slider {
+    constructor(width, height, count) {
+      this.width = width;
+      this.height = height;
+      this.count = count;
+    }
+    nextSlide() {
+      console.log('Moving forward');
+    }
+    prevSlide() {
+      console.log('Moving back');
+    }
+    whoAmI() {
+      console.log(this.width, this.height, this.count);
+    }
+  }
+  
+  class AutoSlider extends Slider {
+    constructor(width, height, count, auto) {
+      super(width, height, count);
+      this.auto = auto;
+    }
+  
+    play() {
+      console.log(`Autoplay: ${this.auto}`);
+    }
+  }
+  
+  // const slider = new AutoSlider(300, 500, 4, true);
+  
+  // slider.whoAmI();
+  // slider.play();
+  
+  
+export {button};
+export default Slider;
---
+++ lesson87 - classes/src/index.js
@@ -0,0 +1,8 @@
+// import {btn, Slider} from './button.js';
+// import * as total from './button.js';
+import Slider, {button} from './button.js';
+
+const slider = new Slider(400, 300, 5); 
+slider.whoAmI();
+
+console.log(button())
