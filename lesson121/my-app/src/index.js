42eebaaeb370cbf53f242186c24990b65b61c7bb
Ivan Zviagin <ivan@ivan.pw>
2022-10-16 00:26:48 +0400

    lesson 122

--- lesson121/my-app/src/index.js
+++ lesson121/my-app/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+const text = 'Hello world';
+
+const elem = (
+  <div>
+    <h2 className="text">Text: {text}</h2>
+    <label htmlFor="input_1"></label>
+    <input type="text"></input>
+    <button tabIndex="0">Click</button>
+  </div>
+);
+
+// const elem = React.createElement(
+//   'h2',
+//   { className: 'greetings' },
+//   'Hello world'
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(elem);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
