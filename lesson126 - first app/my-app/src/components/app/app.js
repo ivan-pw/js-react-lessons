630e3e3b7d9900edf090ca4fc96f4a4d829783f8
Ivan Zviagin <ivan@ivan.pw>
2022-10-18 22:03:58 +0400

    lesson 126 + 128

--- lesson126 - first app/my-app/src/components/app/app.js
+++ lesson126 - first app/my-app/src/components/app/app.js
@@ -6,6 +6,24 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 function App() {
+  const data = [
+    {
+      name: 'John',
+      salary: 800,
+      increase: true,
+    },
+    {
+      name: 'Alex',
+      salary: 3000,
+      increase: true,
+    },
+    {
+      name: 'Carl',
+      salary: 5000,
+      increase: false,
+    },
+  ];
+
   return (
     <div className="app">
       <AppInfo />
@@ -13,7 +31,7 @@ function App() {
         <SearchPanel />
         <AppFilter />
       </div>
-      <EmployeesList />
+      <EmployeesList data={data} />
       <EmployeesAddForm />
     </div>
   );
