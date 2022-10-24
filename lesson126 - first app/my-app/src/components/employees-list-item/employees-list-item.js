6e59f1de3b4c4f982dcc97fcbfdaf537e0250c53
Ivan Zviagin <ivan@ivan.pw>
2022-10-24 20:36:37 +0400

    lesson 126 + 140 - react inline styles

--- lesson126 - first app/my-app/src/components/employees-list-item/employees-list-item.js
+++ lesson126 - first app/my-app/src/components/employees-list-item/employees-list-item.js
@@ -1,7 +1,6 @@
-
 import './employees-list-item.css';
 
-const EmployeesListItem = (props)=> {
+const EmployeesListItem = (props) => {
   const {
     name,
     salary,
@@ -9,7 +8,8 @@ const EmployeesListItem = (props)=> {
     onToggleProp,
     onSalaryChange,
     increase,
-    rise } = props;
+    rise,
+  } = props;
 
   let classNames = 'list-group-item d-flex justify-content-between';
   if (increase) {
@@ -20,11 +20,20 @@ const EmployeesListItem = (props)=> {
   }
 
   return (
-    <li
-      className={classNames}>
+    <li className={classNames}>
       <span
         className="list-group-item-label"
-        onClick={onToggleProp} data-toggle="rise">{name}</span>
+        onClick={onToggleProp}
+        data-toggle="rise"
+        style={{
+          fontSize: '1.5em',
+          color: 'red',
+          transition: 'all',
+          WebkitTransition: 'all',
+        }}
+      >
+        {name}
+      </span>
       <input
         type="text"
         className="list-group-item-input"
@@ -32,16 +41,16 @@ const EmployeesListItem = (props)=> {
         onChange={onSalaryChange}
       />
       <div className="d-flex justify-content-center align-items-center">
-        <button type="button" className="btn-cookie btn-sm "
-          onClick={onToggleProp} data-toggle="increase"
+        <button
+          type="button"
+          className="btn-cookie btn-sm "
+          onClick={onToggleProp}
+          data-toggle="increase"
         >
           <i className="fas fa-cookie"></i>
         </button>
 
-        <button
-          type="button"
-          className="btn-trash btn-sm "
-          onClick={onDelete}>
+        <button type="button" className="btn-trash btn-sm " onClick={onDelete}>
           <i className="fas fa-trash"></i>
         </button>
         <i className="fas fa-star"></i>
