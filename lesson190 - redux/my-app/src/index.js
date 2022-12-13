d316ab3c53d955ff380519f92ae2d9bee19703ec
Ivan Zviagin <ivan@ivan.pw>
2022-12-14 00:58:40 +0400

    lesson 193

--- lesson190 - redux/my-app/src/index.js
+++ lesson190 - redux/my-app/src/index.js
@@ -1,67 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { configureStore, createStore, bindActionCreators } from 'redux';
+import { createStore } from 'redux';
 
-import * as actions from './actions';
-// import { rnd, dec, inc } from './actions';
+import { Provider } from 'react-redux';
 import { reducer } from './reducer';
 
+import App from './components/App';
+
 import './index.css';
-import App from './App';
+// import App from './App';
 
 const store = createStore(reducer);
 
-const { dispatch, subscribe, getState } = store;
-
-const update = () => {
-  document.getElementById('counter').textContent = getState().value;
-};
-
-subscribe(update);
-// store.subscribe(() => {
-//   console.log(store.getState());
-// });
-
-// const bindActionCreator =
-//   (creator, dispatch) =>
-//   (...args) => {
-//     dispatch(creator(...args));
-//   };
-
-const { inc, dec, rnd } = bindActionCreators(
-  actions,
-  // {
-  //   incDispatch: actions.inc,
-  //   decDispatch: actions.dec,
-  //   rndDispatch: actions.rnd,
-  // },
-  dispatch
-);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-document.getElementById('inc').addEventListener('click', inc);
-document.getElementById('dec').addEventListener('click', dec);
-document
-  .getElementById('rnd')
-  .addEventListener('click', () => rnd(Math.floor(Math.random() * 10)));
-
-// document.getElementById('rnd').addEventListener('click', (e) => {
-//   const value = Math.floor(Math.random() * 10);
-//   store.dispatch({ type: 'RND', payload: value });
-// });
-
-// console.log(store.getState());
-dispatch({ type: 'INC' });
-dispatch({ type: 'INC' });
-// console.log(store.getState());
-
-// let state = reducer(initialState, { type: 'INC' });
-// console.log(state);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
